917d920905743c6c1b19b862e336fa0d637a35b7
lixiong <qqlixiongjob@163.com>
2020-10-29 21:45:03 +0800

    MVVM基础框架 去除代码混淆。

--- app/src/main/java/com/lx/mvvm/MainActivity.java
+++ app/src/main/java/com/lx/mvvm/MainActivity.java
@@ -1,17 +1,14 @@
 package com.lx.mvvm;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.lx.framework.base.BaseActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
     }
 }
