d5828f6a0221e905aaa3310c7db099141133d2ff
lixiong <qqlixiongjob@163.com>
2020-11-12 12:03:44 +0800

    去掉了文件预览。

--- app/src/main/java/com/lx/mvvm/MainActivity.java
+++ app/src/main/java/com/lx/mvvm/MainActivity.java
@@ -23,29 +23,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        try {
-            FileUtils.copyAssetsDir(Utils.getContext(), "tbs", FILE_DIR);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void openfile(View view) {
-        openFileWithTbs(getFilePath("abc.pdf"));
-    }
-
-    @NonNull
-    private String getFilePath(String fileName) {
-        return new File(FILE_DIR + fileName).getAbsolutePath();
-    }
-
-    private void openFileWithTbs(String filePath) {
-        KLog.d("lixiong", "Open File: " + filePath);
-//        startFragment(TBSFileViewFragment.getInstance(filePath));
-        AttachPreviewActivity.startActivity(this,filePath);
-    }
 }
