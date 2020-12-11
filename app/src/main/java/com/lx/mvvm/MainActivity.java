44b601d18fd07cf37ef8b24c415f3b63d4c08e52
lixiong <qqlixiongjob@163.com>
2020-12-11 22:48:12 +0800

    ...

--- app/src/main/java/com/lx/mvvm/MainActivity.java
+++ app/src/main/java/com/lx/mvvm/MainActivity.java
@@ -1,20 +1,8 @@
 package com.lx.mvvm;
 
 import android.os.Bundle;
-import android.view.View;
 
-import com.artifex.mupdflib.ui.AttachPreviewActivity;
-import com.lx.framework.utils.FileUtils;
-import com.lx.framework.utils.KLog;
-import com.lx.framework.utils.Utils;
-
-import java.io.File;
-import java.io.IOException;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import static com.lx.mvvm.MyApp.FILE_DIR;
 
 public class MainActivity extends AppCompatActivity {
 
