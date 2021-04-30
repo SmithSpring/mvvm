e0684cbae22438c65e352fb398b88109f326ea69
lixiong <qqlixiongjob@163.com>
2021-04-30 10:51:45 +0800

    添加了状态栏的背景颜色

--- mvvm-framework/src/main/java/com/lx/framework/base/BaseActivity.java
+++ mvvm-framework/src/main/java/com/lx/framework/base/BaseActivity.java
@@ -16,6 +16,7 @@ import android.view.WindowManager;
 
 import com.gyf.immersionbar.ImmersionBar;
 import com.lx.framework.BR;
+import com.lx.framework.R;
 import com.lx.framework.bus.event.eventbus.EventBusUtil;
 import com.lx.framework.bus.event.eventbus.MessageEvent;
 import com.mumu.dialog.MMLoading;
@@ -97,6 +98,7 @@ public abstract class BaseActivity<V extends ViewDataBinding, VM extends BaseVie
         //在BaseActivity里初始化
         mImmersionBar = ImmersionBar.with(this)
                 .statusBarDarkFont(statusBarDarkFont())    //默认状态栏字体颜色为黑色
+                .statusBarColor(statusBarColor())
                 .keyboardEnable(false, WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN
                         | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);  //解决软键盘与底部输入框冲突问题，默认为false，还有一个重载方法，可以指定软键盘mode
         //必须设置View树布局变化监听，否则软键盘无法顶上去，还有模式必须是SOFT_INPUT_ADJUST_PAN
@@ -111,6 +113,10 @@ public abstract class BaseActivity<V extends ViewDataBinding, VM extends BaseVie
      */
     protected boolean isRegisterEventBus() {
         return false;
+    }
+
+    public int statusBarColor(){
+        return R.color.white;
     }
 
     /**
