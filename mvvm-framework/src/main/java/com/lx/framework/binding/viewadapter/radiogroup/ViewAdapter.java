b91c1f2708e2397cc17c21f5aaf2b1d9b9bf98e5
lixiong <qqlixiongjob@163.com>
2021-05-26 13:06:49 +0800

    ......

--- mvvm-framework/src/main/java/com/lx/framework/binding/viewadapter/radiogroup/ViewAdapter.java
+++ mvvm-framework/src/main/java/com/lx/framework/binding/viewadapter/radiogroup/ViewAdapter.java
@@ -11,12 +11,15 @@ import com.lx.framework.binding.command.BindingCommand;
  * Created by lx on 2017/6/18.
  */
 public class ViewAdapter {
-    @BindingAdapter(value = {"onCheckedChangedCommand"}, requireAll = false)
-    public static void onCheckedChangedCommand(final RadioGroup radioGroup, final BindingCommand<String> bindingCommand) {
+    @BindingAdapter(value = {"onCheckedChangedCommand","isRepeat"}, requireAll = false)
+    public static void onCheckedChangedCommand(final RadioGroup radioGroup, final BindingCommand<String> bindingCommand,final boolean isRepeat) {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
                 RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+                if (isRepeat && !radioButton.isPressed()){
+                    return;
+                }
                 bindingCommand.execute(radioButton.getText().toString());
             }
         });
