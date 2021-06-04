0ec0e41509f7441349107365862bd12b0ca3e89b
lixiong <qqlixiongjob@163.com>
2021-06-04 13:23:50 +0800

    ......

---
+++ mvvm-framework/src/main/java/com/lx/framework/utils/phone/PhoneUtils.java
@@ -0,0 +1,21 @@
+package com.lx.framework.utils.phone;
+
+import android.content.Intent;
+import android.net.Uri;
+
+import com.lx.framework.utils.Utils;
+
+public class PhoneUtils {
+    private static void callPhone(String phoneNumber) {
+        call(Intent.ACTION_CALL, "tel:",phoneNumber);
+    }
+
+    private static void sendSMS(String phoneNumber) {
+        call(Intent.ACTION_SENDTO, "smsto:",phoneNumber);
+    }
+
+    private static void call(String actionCall, String s,String phoneNumber) {
+        Intent intent_call = new Intent(actionCall, Uri.parse(s + phoneNumber));
+        Utils.getContext().startActivity(intent_call);
+    }
+}
